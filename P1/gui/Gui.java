9280bf77a4f8b630dba403b34f417a7360fa965a
hanon11 <lucia.atienza2@gmail.com>
2022-02-09 22:58:30 +0100

    P1

--- P1/gui/Gui.java
+++ P1/gui/Gui.java
@@ -1,39 +1,57 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-public class Gui extends JFrame {
+
+public class Gui extends JFrame 
+{
     private static String botones[] = 
     { 
-        "Parámetros","Curva","Computar", 
-        "Detener"
+        "Parámetros","Curva","Computar", "Detener"
     };
-    public Gui(String nombre) {
+    private static String menus[] = 
+    { 
+        "Opcion A","Opcion B","Opcion C", "Acerca de"
+    };
+
+    
+
+    private JPanel crearPanelBotones()
+    {
+        JPanel panelBotones = new JPanel();
+        panelBotones.setLayout(new BoxLayout(panelBotones, BoxLayout.Y_AXIS));
+
+        JButton b1;
+        for(int i = 0; i < botones.length; i++)
+        {
+            b1 = new JButton(botones[i]);
+            b1.setAlignmentX(Component.CENTER_ALIGNMENT); //botones centrados en el panel
+            b1.setMinimumSize(new Dimension(150,30)); //establezco tam de los botones para que sean iguales
+            b1.setMaximumSize(new Dimension(150,30));
+            b1.addActionListener(new escuchaClick()); 
+            panelBotones.add(b1);
+        }
+        return panelBotones;
+    }
+
+    public Gui(String nombre) 
+    {
         super(nombre);
         //menu superior
         JMenuBar jmb = new JMenuBar();
         setJMenuBar(jmb);
-        JMenu A = new JMenu("Opcion A");
-        JMenu B = new JMenu("Opcion B");
-        JMenu C = new JMenu("Opcion C");
-        JMenu info = new JMenu("Acerca de");
-        jmb.add(A);
-        jmb.add(B);
-        jmb.add(C);
-        jmb.add(info);
-        setLayout(new BorderLayout(5, 10));
-        JPanel jp = new JPanel();
-        //add(jp);
-        //botones
-        JPanel panelDerecho = new JPanel();
-        panelDerecho.setLayout(new BoxLayout(panelDerecho, BoxLayout.Y_AXIS));
         for(int i = 0; i < botones.length; i++)
-            panelDerecho.add(new JButton(botones[i]));
-        
+            jmb.add(new JMenu(menus[i]));
+
+
+        //panel botones
+        add(crearPanelBotones());
+
         JButton image = new JButton("", new ImageIcon(getClass().getResource("Raspberry.png")));
         add(BorderLayout.WEST, image);
-        add(panelDerecho);
     }
-    public static void main(String args[]) {
+
+    public static void main(String args[]) 
+    {
         Gui frame = new Gui("Mi primera interfaz gráfica");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
         frame.setSize(700, 600);
