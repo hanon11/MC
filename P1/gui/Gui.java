030d3f9443469108032b9b162b7164f0a0238adf
hanon11 <lucia.atienza2@gmail.com>
2022-02-12 20:45:19 +0100

    P1

--- P1/gui/Gui.java
+++ P1/gui/Gui.java
@@ -4,49 +4,109 @@ import javax.swing.*;
 
 public class Gui extends JFrame 
 {
+    static JFrame f;
+    private ButtonListener bl = new ButtonListener();
+
+    private ActionListener al = new ActionListener() 
+    {  
+        public void actionPerformed(ActionEvent e) 
+        {
+            String name =  ((JMenuItem)e.getSource()).getText();  
+            JDialog d = new JDialog(f, name);
+            JLabel l = new JLabel(name);
+            d.add(l);
+            d.setSize(300, 150);
+            d.setVisible(true);
+        }
+    };
+
     private static String botones[] = 
     { 
         "Parámetros","Curva","Computar", "Detener"
     };
+
     private static String menus[] = 
     { 
         "Opcion A","Opcion B","Opcion C", "Acerca de"
     };
+    private static String itemsMenu[] = 
+    { 
+        "Sección 1A","Sección 1B", "Sección 1C", "Ayuda"
+    };
 
-    
+    //crea nuevo JDialog por cada boton
+    class ButtonListener implements ActionListener 
+    {
+        public void actionPerformed(ActionEvent e) 
+        {
+            String name = ((JButton)e.getSource()).getText();  
+            JDialog d = new JDialog(f, name);
+            JLabel l = new JLabel(name);
+            d.add(l);
+            d.setSize(300, 150);
+            d.setVisible(true);
+        }
+    }
 
     private JPanel crearPanelBotones()
     {
         JPanel panelBotones = new JPanel();
-        panelBotones.setLayout(new BoxLayout(panelBotones, BoxLayout.Y_AXIS));
-
-        JButton b1;
-        for(int i = 0; i < botones.length; i++)
+        SpringLayout layout = new SpringLayout();
+        panelBotones.setLayout(layout);
+        
+        JButton b1 = new JButton(botones[0]), b2;
+        b1.addActionListener(bl);
+        b1.setPreferredSize(new Dimension(150, 27));
+        panelBotones.add(b1);
+        layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, b1, 0, SpringLayout.HORIZONTAL_CENTER, panelBotones);
+        layout.putConstraint(SpringLayout.NORTH, b1, 30, SpringLayout.NORTH, panelBotones);
+        
+        for(int i = 1; i < botones.length; i++)
         {
-            b1 = new JButton(botones[i]);
-            b1.setAlignmentX(Component.CENTER_ALIGNMENT); //botones centrados en el panel
-            b1.setMinimumSize(new Dimension(150,30)); //establezco tam de los botones para que sean iguales
-            b1.setMaximumSize(new Dimension(150,30));
-            b1.addActionListener(new escuchaClick()); 
-            panelBotones.add(b1);
+            b2 = new JButton(botones[i]);
+            b2.setPreferredSize(new Dimension(150, 27));
+            b2.addActionListener(bl);
+            
+            panelBotones.add(b2);
+            layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, b2, 0, SpringLayout.HORIZONTAL_CENTER, b1);
+            layout.putConstraint(SpringLayout.NORTH, b2, 45, SpringLayout.NORTH, b1);
+            b1 = b2;
         }
         return panelBotones;
     }
 
+    private JMenuBar SimpleMenus()
+    {
+        JMenuBar mb = new JMenuBar();
+        JMenu menu;
+        JMenuItem item;
+        for(int i = 0; i < itemsMenu.length; i++) 
+        {
+            //itemsMenu[i].addActionListener(al);
+            menu = new JMenu(menus[i]);
+            menu.add(itemsMenu[i]).addActionListener(al);
+            mb.add(menu);
+        }
+        
+        return mb;
+    }
+
+
     public Gui(String nombre) 
     {
         super(nombre);
+        f = new JFrame(nombre);
         //menu superior
-        JMenuBar jmb = new JMenuBar();
+        /*JMenuBar jmb = new JMenuBar();
         setJMenuBar(jmb);
         for(int i = 0; i < botones.length; i++)
-            jmb.add(new JMenu(menus[i]));
+            jmb.add(new JMenu(menus[i]));*/
 
-
+        setJMenuBar(SimpleMenus());
         //panel botones
         add(crearPanelBotones());
 
-        JButton image = new JButton("", new ImageIcon(getClass().getResource("Raspberry.png")));
+        JButton image = new JButton("", new ImageIcon(getClass().getResource("descarga.jfif")));
         add(BorderLayout.WEST, image);
     }
 
