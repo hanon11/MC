59d821b2f3acd068510d0374b557af4e94bbda8d
hanon11 <lucia.atienza2@gmail.com>
2022-04-25 18:20:59 +0200

    P8

--- P8/tumoralGrowth.java
+++ P8/tumoralGrowth.java
@@ -6,14 +6,15 @@ import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.util.Arrays;
 import javax.swing.JSpinner;
 
 
 public class tumoralGrowth extends JFrame
 {
-    private static int tam = 500, n_generaciones = 2,i=0;
-    private static double Ps = 1, Pp = 0.25, NP = 1, Pm = 0.2 ,PH =0, P1, P2,P3,P4;
-    private static int[] celulasVivas = new int[tam];
+    private static int tam = 400, n_generaciones = 2, indice =0;
+    private static double Ps = 1, Pp = 0.25, NP = 1, Pm = 0.2 ,PH = 0, P1, P2,P3,P4;
+    private static int[] celulasVivas = new int[n_generaciones];
     private static JFrame f;
     private static String[] menus = { "Opcion A","Opcion B","Opcion C", "Acerca de" };
     private static String[] itemsMenu = { "Seccion 1A","Seccion 1B", "Seccion 1C", "Ayuda" };
@@ -31,7 +32,7 @@ public class tumoralGrowth extends JFrame
             for ( int j = 0; j < tam; j++ )
                 t_1[i][j] = 0;
         }
-
+        t_1[tam/2][tam/2] = 1;
         for ( int i = 0; i < tam; i++ ) //una vez hechos todos los cambios, actualizamos para la proxima iteracion
         {
             System.arraycopy(t_1[i], 0, actual[i], 0, tam);
@@ -43,6 +44,7 @@ public class tumoralGrowth extends JFrame
     public static void reset()
     {
         rellenar_matrices();
+        indice = 0;
     }
 
     // ======================================== DIBUJO AUTOMATA ====================================================================
@@ -71,14 +73,57 @@ public class tumoralGrowth extends JFrame
 
         public void proliferation(int i, int j)
         {
-            P1 = (double)(1-t_1[i-1][j])/(4-(t_1[i+1][j]+t_1[i-1][j]+t_1[i][j+1]+t_1[i][j-1]));
-            P2 = (double)(1-t_1[i+1][j])/(4-(t_1[i+1][j]+t_1[i-1][j]+t_1[i][j+1]+t_1[i][j-1]));
-            P3 = (double)(1-t_1[i][j-1])/(4-(t_1[i+1][j]+t_1[i-1][j]+t_1[i][j+1]+t_1[i][j-1]));
-            P4 = (double)(1-t_1[i][j+1])/(4-(t_1[i+1][j]+t_1[i-1][j]+t_1[i][j+1]+t_1[i][j-1]));
+            int sumatorio = 0, numerador1, numerador2, numerador3, numerador4;
+            if( i-1 == -1 ) //se pasa por la izq
+            {
+                sumatorio += t_1[tam - 1][j];
+                numerador1 = tam-1;
+            }
+            else
+            {
+                sumatorio += t_1[i - 1][j];
+                numerador1 = i-1;
+            }
+
+            if( i+1 == tam )//se pasa por la dcha
+            {
+                sumatorio += t_1[0][j];
+                numerador2 = 0;
+            }
+            else {
+                sumatorio += t_1[i + 1][j];
+                numerador2 = i+1;
+            }
+
+            if( j-1 == -1 ) //se pasa por arriba
+            {
+                sumatorio += t_1[i][tam - 1];
+                numerador3 = tam-1;
+            }
+            else {
+                sumatorio += t_1[i][j - 1];
+                numerador3 = j-1;
+            }
+
+            if( j+1 == tam )//se pasa por abajo
+            {
+                sumatorio += t_1[i][0];
+                numerador4 = 0;
+            }
+            else {
+                sumatorio += t_1[i][j + 1];
+                numerador4 = j+1;
+            }
+
+            P1 = (double)(1-t_1[numerador1][j])/(4-sumatorio);
+            P2 = (double)(1-t_1[numerador2][j])/(4-sumatorio);
+            P3 = (double)(1-t_1[i][numerador3])/(4-sumatorio);
+            P4 = (double)(1-t_1[i][numerador4])/(4-sumatorio);
         }
 
         public void funcionTransicion()
         {
+            int celulas = 0;
             for ( int i = 0; i < tam; i++ )
             {
                 for (int j = 0; j < tam; j++)
@@ -86,45 +131,116 @@ public class tumoralGrowth extends JFrame
                     double rrp = Math.random();
                     double rr = Math.random();
                     double rrm = Math.random();
-                    if( rr >= Ps )
-                        actual[i][j] = 0;
-                    else
+                    if (actual[i][j]==1)
                     {
-                        if( rrp >= Pp )
-                        {
-                            if( rrm >= Pm )
+                        if (rr >= Ps) //la celula muere
+                            actual[i][j] = 0;
+                        else {
+                            celulas++;
+                            proliferation(i, j); //sirve tanto para proliferar como para migrar
+                            if (rrp >= Pp) //no proliferacion
                             {
-                                //quiescent
-                            }
-                            else
-                            {
-                                //migration
-                            }
-                        }
-                        else
-                        {
-                            PH++;
-                            if( PH < NP )
-                            {
-                                if( rrp >= Pp )
-                                {
-                                    if( rrm >= Pm )
-                                    {
-                                        //quiescent
+                                if (rrm < Pm) {
+                                    rr = Math.random();
+                                    if (rr <= P1) {
+                                        actual[i][j] = 0;
+                                        if (i - 1 == -1)
+                                            actual[tam - 1][j] = 1;
+                                        else
+                                            actual[i - 1][j] = 1; //hija
+                                    } else if (rr <= P1 + P2) {
+                                        actual[i][j] = 0;
+                                        if (i + 1 == tam)
+                                            actual[0][j] = 1;
+                                        else
+                                            actual[i + 1][j] = 1; //hija
+                                    } else if (rr <= P1 + P2 + P3) {
+                                        actual[i][j] = 0;
+                                        if (j - 1 == -1)
+                                            actual[i][tam - 1] = 1;
+                                        else
+                                            actual[i][j - 1] = 1; //hija
+                                    } else {
+                                        actual[i][j] = 0;
+                                        if (j + 1 == tam)
+                                            actual[i][0] = 1;
+                                        else
+                                            actual[i][j + 1] = 1; //hija
                                     }
-                                    else
-                                    {
-                                        //migration
-                                    }
+                                    celulas++;
                                 }
-                            }
-                            else
-                            {
-                                //proliferation
+                            } else {
+                                PH++;
+                                rr = Math.random();
+                                if (PH < NP) //migra sin proliferar
+                                {
+                                    if (rrm >= Pm) {
+                                        //quiescent
+                                    } else {
+                                        if (rr <= P1) {
+                                            actual[i][j] = 0;
+                                            if (i - 1 == -1)
+                                                actual[tam - 1][j] = 1;
+                                            else
+                                                actual[i - 1][j] = 1; //hija
+                                        } else if (rr <= P1 + P2) {
+                                            actual[i][j] = 0;
+                                            if (i + 1 == tam)
+                                                actual[0][j] = 1;
+                                            else
+                                                actual[i + 1][j] = 1; //hija
+                                        } else if (rr <= P1 + P2 + P3) {
+                                            actual[i][j] = 0;
+                                            if (j - 1 == -1)
+                                                actual[i][tam - 1] = 1;
+                                            else
+                                                actual[i][j - 1] = 1; //hija
+                                        } else {
+                                            actual[i][j] = 0;
+                                            if (j + 1 == tam)
+                                                actual[i][0] = 1;
+                                            else
+                                                actual[i][j + 1] = 1; //hija
+                                        }
+                                        celulas++;
+                                    }
+                                } else //proliferation
+                                {
+                                    rr = Math.random();
+                                    if (rr <= P1) {
+                                        if (i - 1 == -1)
+                                            actual[tam - 1][j] = 1;
+                                        else
+                                            actual[i - 1][j] = 1; //hija
+                                    } else if (rr <= P1 + P2) {
+                                        if (i + 1 == tam)
+                                            actual[0][j] = 1;
+                                        else
+                                            actual[i + 1][j] = 1; //hija
+                                    } else if (rr <= P1 + P2 + P3) {
+                                        if (j - 1 == -1)
+                                            actual[i][tam - 1] = 1;
+                                        else
+                                            actual[i][j - 1] = 1; //hija
+                                    } else {
+                                        if (j + 1 == tam)
+                                            actual[i][0] = 1;
+                                        else
+                                            actual[i][j + 1] = 1; //hija
+                                    }
+                                    celulas++;
+                                    PH = 0;
+                                }
                             }
                         }
                     }
                 }
+            }
+            celulasVivas[indice] = celulas;
+            indice++;
+            for ( int i = 0; i < tam; i++ ) //una vez hechos todos los cambios, actualizamos para la proxima iteracion
+            {
+                System.arraycopy(actual[i], 0, t_1[i], 0, tam);
             }
         }
 
@@ -138,7 +254,7 @@ public class tumoralGrowth extends JFrame
                 {
                     for (int j = 0; j < alto; j++)
                     {
-                        g.setColor(Color.BLACK);
+                        g.setColor(Color.WHITE);
                         g.drawOval(i, j, 1, 1);
                     }
                 }
@@ -151,7 +267,7 @@ public class tumoralGrowth extends JFrame
                             g.drawOval(i, j, 2, 2);
                         } else {
                             g.setColor(Color.BLUE);
-                            g.drawOval(i, j, 2, 2);
+                            g.drawOval(i, j, 3, 3);
                         }
                     }
                 }
@@ -165,7 +281,6 @@ public class tumoralGrowth extends JFrame
         private int[] dat;
         private int puntos;
         private int[] Xdat;
-        private int[] Ydat;
 
         public grafPobCelular()
         {
@@ -173,45 +288,34 @@ public class tumoralGrowth extends JFrame
             dat = celulasVivas;
             puntos = dat.length;
             Xdat = new int[puntos];
-            Ydat = new int[puntos];
-            //separamos puntos para coordenadas x e y...
-            for (int i = 0; i < puntos; i++)
-            {
-                Xdat[i] = i;
-                Ydat[i] = dat[i];
-            }
         }
         public void paint(Graphics g)
         {
             Image img = createImageWithText2();
-            g.drawImage(img, 5,90,this);
+            g.drawImage(img, 5,5,this);
         }
 
         public Image createImageWithText2()
         {
-            dat = celulasVivas;
-            puntos = dat.length;
             Xdat = new int[puntos];
-            Ydat = new int[puntos];
             int ancho = tam;
-            int alto = tam;
+            int alto = tam+tam;
             BufferedImage bufferedImage = new BufferedImage(ancho, alto, BufferedImage.TYPE_INT_RGB);
             g = bufferedImage.getGraphics();
             for (int i = 0; i < puntos; i++)
             {
-                Xdat[i] = i;
-                Ydat[i] = dat[i];
+                Xdat[i] = 3*i;
             }
+            //System.out.println(Arrays.toString(celulasVivas));
             Graphics2D g2 = (Graphics2D) g;
             g2.setColor(Color.BLUE);
             g2.setStroke(new BasicStroke(3));
-
             for (int i = 0; i < puntos-1; i++)
             {
                 int x0 = Xdat[i];
                 int x1 = Xdat[i + 1];
-                int y0 = tam - Ydat[i];
-                int y1 = tam - Ydat[i + 1];
+                int y0 = alto - celulasVivas[i];
+                int y1 = alto - celulasVivas[i + 1];
                 g2.drawLine( x0, y0, x1, y1 );
             }
             return bufferedImage;
@@ -228,6 +332,7 @@ public class tumoralGrowth extends JFrame
             String name = ((JButton) e.getSource()).getText();
             if (name.equals("Inicio"))
             {
+                indice=0;
                 rellenar_matrices();
                 for (int i = 0; i < n_generaciones; i++)
                 {
@@ -237,7 +342,7 @@ public class tumoralGrowth extends JFrame
                 {
                     JDialog d = new JDialog(f, "Evolucion grafica del estado de la reticula");
                     d.add(new grafPobCelular());
-                    d.setSize(800, 850);
+                    d.setSize(1200, 1000);
                     d.setVisible(true);
                 }
             }
@@ -280,7 +385,11 @@ public class tumoralGrowth extends JFrame
 
         JSpinner spinner2 = new JSpinner(new SpinnerNumberModel(1, 1, 100000, 1));
         spinner2.setSize(70, 10);
-        spinner2.addChangeListener(e -> n_generaciones = (int) spinner2.getValue());
+        spinner2.addChangeListener(e ->
+        {
+            n_generaciones = (int) spinner2.getValue();
+            celulasVivas = new int[n_generaciones];
+        });
 
         panelBotones.add(spinner2);
         layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, spinner2, 0, SpringLayout.HORIZONTAL_CENTER, label3);
@@ -339,7 +448,7 @@ public class tumoralGrowth extends JFrame
 
     public static void main(String[] args)
     {
-        tumoralGrowth frame = new tumoralGrowth("Conway's Game of Life");
+        tumoralGrowth frame = new tumoralGrowth("Simulacion de Crecimiento Tumoral con AC 2-D");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1000, 800);
         frame.setVisible(true);
